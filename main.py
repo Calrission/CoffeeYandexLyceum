96dd8a115737866585aa9a8aa1e598f4879e3694
Kavrese <va2005art@gmail.com>
2022-11-21 18:38:22 +0300

    add db file

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from sys import argv, exit
+from PyQt5 import uic
 
+
+class Main(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("main.ui", self)
+        self.initUI()
+
+    def initUI(self):
+        pass
+
+
+if __name__ == "__main__":
+    app = QApplication(argv)
+    main = Main()
+    main.show()
+    exit(app.exec())
